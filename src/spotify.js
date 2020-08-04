cc5535eef004558574e4fd998dcf41cf5c81e2dc
Ankit Hans <ankithans1947@gmail.com>
2020-08-04 13:03:52 +0530

    connected spotify login

---
+++ src/spotify.js
@@ -0,0 +1,30 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+const redirectUri = 'http://localhost:3000/';
+const clientId = '593970ff7b1e4efeb01081dee25ccd1c';
+
+const scopes = [
+  'user-read-currently-playing',
+  'user-read-recently-played',
+  'user-read-playback-state',
+  'user-top-read',
+  'user-modify-playback-state',
+];
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial, item) => {
+      // #accessToken=mysupersecretkey&name=sonny
+      let parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  '%20'
+)}&response_type=token&show_dialog=true`;
