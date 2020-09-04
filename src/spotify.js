3629371208c32581b7aed27d8bf2166097a87743
Ankit Hans <ankithans1947@gmail.com>
2020-09-04 10:41:28 +0530

    final pages

--- src/spotify.js
+++ src/spotify.js
@@ -1,30 +1,28 @@
 // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
-
-export const authEndpoint = 'https://accounts.spotify.com/authorize';
-const redirectUri = 'http://localhost:3000/';
-const clientId = '593970ff7b1e4efeb01081dee25ccd1c';
-
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "cff76213089a4e228206c8bf120d67cb";
+const redirectUri = "http://localhost:3000/";
 const scopes = [
-  'user-read-currently-playing',
-  'user-read-recently-played',
-  'user-read-playback-state',
-  'user-top-read',
-  'user-modify-playback-state',
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
 ];
 
-export const getTokenFromUrl = () => {
+export const getTokenFromResponse = () => {
   return window.location.hash
     .substring(1)
-    .split('&')
+    .split("&")
     .reduce((initial, item) => {
-      // #accessToken=mysupersecretkey&name=sonny
-      let parts = item.split('=');
+      var parts = item.split("=");
       initial[parts[0]] = decodeURIComponent(parts[1]);
 
       return initial;
     }, {});
 };
 
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-  '%20'
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
 )}&response_type=token&show_dialog=true`;
